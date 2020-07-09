6030fcc4bce99d1d42d2c831371806dc7992bba8
FayeCarter <fayecarter333@gmail.com>
2020-07-09 18:17:01 +0100

    Display Popular movie poster image

--- client/src/components/Movie.js
+++ client/src/components/Movie.js
@@ -3,6 +3,7 @@ import React from 'react';
 export const Movie = ({ movie }) => {
   return (
     <div data-testid="movie" >
+      <img data-testid="movie-image" src={`http://image.tmdb.org/t/p/w185/${ movie.image }`} alt="poster"/>
       <div data-testid="movie-title" >{ movie.title }</div>
       <div data-testid="movie-about" >{ movie.about }</div>
       <div data-testid="movie-rating" >{ movie.rating }</div>
