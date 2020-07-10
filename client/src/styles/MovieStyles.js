9f6638ba87115841b2825869ed63c298be22a1b6
FayeCarter <fayecarter333@gmail.com>
2020-07-10 11:41:40 +0100

    Add min width to movies

--- client/src/styles/MovieStyles.js
+++ client/src/styles/MovieStyles.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const MovieWrapper = styled.section`
   width: 15%;
+  min-width: 170px;
   background-color: rgb(248,248,248);
   margin: 5px;  
   box-sizing: border-box;
