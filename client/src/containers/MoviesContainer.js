bdd6ced90ec45a512272425687c0ccdae792fd34
FayeCarter <fayecarter333@gmail.com>
2020-07-10 10:30:18 +0100

    Add Styled Movies Container

--- client/src/containers/MoviesContainer.js
+++ client/src/containers/MoviesContainer.js
@@ -2,16 +2,17 @@ import React from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { Movie } from "../components/Movie";
 import { GET_POPULAR_MOVIES } from "../graphql/get-popular-movies";
+import { MoviesContainerWrapper} from "../styles/MoviesContainerStyles";
 
 export const MoviesContainer = () => {
   const{ data: { popularMovies = [] } = {} } = useQuery(GET_POPULAR_MOVIES)
   return (
     <>
-      <div data-test-id="movies-container">
-        <h1>Movies</h1>
+      <h1>Movies</h1>
+      <MoviesContainerWrapper data-test-id="movies-container">
         {popularMovies && popularMovies
           .map(movie => <Movie key={movie.id} movie={movie} />)}
-      </div>
+      </MoviesContainerWrapper>
     </>
   );
 };
---
+++ client/src/styles/MoviesContainerStyles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const MoviesContainerWrapper = styled.main`
+  max-width: 1140px;
+  padding: 20px 15px;
+  margin: auto;
+  display: flex;
+  flex-wrap: wrap;
+`;
