d7d9657c9eab41a72e2076514c41dc9bf63c8547
FayeCarter <fayecarter333@gmail.com>
2020-07-10 10:26:17 +0100

    Add Styled Movie

--- client/src/components/Movie.js
+++ client/src/components/Movie.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { MovieWrapper} from "../styles/MovieStyles";
 
 export const Movie = ({ movie }) => {
   return (
-    <div data-testid="movie" >
+    <MovieWrapper data-testid="movie" >
       <img data-testid="movie-image" src={`http://image.tmdb.org/t/p/w185/${ movie.image }`} alt="poster"/>
       <div data-testid="movie-title" >{ movie.title }</div>
       <div data-testid="movie-about" >{ movie.about }</div>
       <div data-testid="movie-rating" >{ movie.rating }</div>
-    </div>
+    </MovieWrapper>
   );
 };
---
+++ client/src/styles/MovieStyles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const MovieWrapper = styled.section`
+  width: 15%;
+  background-color: rgb(248,248,248);
+  margin: 5px;  
+  box-sizing: border-box;
+`;
